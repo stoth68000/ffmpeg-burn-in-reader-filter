dbdeaad09934314ef900eb9dd6d7c4c0c7890ca9
Steven Toth <stoth@kernellabs.com>
2017-03-30 17:25:59 -0400

    Added a filter tthat can analyze burned in KL 32bit counters.

--- libavfilter/allfilters.c
+++ libavfilter/allfilters.c
@@ -157,6 +157,7 @@ static void register_all(void)
     REGISTER_FILTER(COLORSPACE,     colorspace,     vf);
     REGISTER_FILTER(CONVOLUTION,    convolution,    vf);
     REGISTER_FILTER(COPY,           copy,           vf);
+    REGISTER_FILTER(KLBURNIN,       klburnin,       vf);
     REGISTER_FILTER(COREIMAGE,      coreimage,      vf);
     REGISTER_FILTER(COVER_RECT,     cover_rect,     vf);
     REGISTER_FILTER(CROP,           crop,           vf);
